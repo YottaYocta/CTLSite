3a6ee1866455a56543920b669f5b0ff56f83ff26
yottayocta <chubbean.rq@gmail.com>
2026-02-20 15:53:24 -0500

    task: button interaction

--- src/components/ExpandableButton.tsx
+++ src/components/ExpandableButton.tsx
@@ -6,54 +6,51 @@ interface ExpandableButtonProps {
 
 export const ExpandableButton: FC<ExpandableButtonProps> = ({ text }) => {
   const [isOpen, setIsOpen] = useState(false);
-
-  useEffect(() => {
-    const minimizeButton = () => {
-      setIsOpen(false);
-    };
-
-    document.addEventListener("mousedown", minimizeButton);
-
-    return () => {
-      document.removeEventListener("mousedown", minimizeButton);
-    };
-  }, []);
-
   return (
-    <div
-      className={
-        (isOpen
-          ? "w-48 h-28 pointer-events-none -translate-x-4 translate-y-4"
-          : "w-8 h-8" + " flex flex-col gap-2") +
-        " " +
-        "bg-white border-neutral-100 border rounded-md flex flex-col items-center justify-center transition-all overflow-clip duration-300 ease-[cubic-bezier(0.25,1,0.5,1)] shadow-sm kode-mono"
-      }
-    >
-      <span
-        className={
-          "text-sm flex flex-col gap-4 p-4 justify-center w-48 h-32" +
-          " " +
-          `${isOpen ? "opacity-100" : "opacity-0 hidden"}`
-        }
-      >
-        <p className="text-neutral-400">Credits:</p>
-        <div className="flex flex-col gap-2">
-          {text.map((str) => (
-            <p>{str}</p>
-          ))}
-        </div>
-      </span>
-
+    <div className="relative flex">
       <button
-        className={
-          "w-full h-full bg-white hover:bg-neutral-50 cursor-pointer" +
-          " " +
-          `${isOpen ? "opacity-0 hidden" : "opacity-100"}`
-        }
-        onClick={() => setIsOpen(true)}
+        className={"button w-8 h-8 p-0 z-10"}
+        onClick={() => setIsOpen(!isOpen)}
       >
-        +
+        <span
+          className={`${isOpen ? "rotate-0" : "rotate-45"} transition-transform flex justify-center items-center`}
+        >
+          <svg
+            className="w-4 h-4 text-neutral-500"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <line x1="18" y1="6" x2="6" y2="18" />
+            <line x1="6" y1="6" x2="18" y2="18" />
+          </svg>
+        </span>
       </button>
+      <div
+        className={
+          (isOpen
+            ? "w-52 h-32 right-12 top-0 shadow-xs bg-white"
+            : "w-8 h-8  right-0 top-0 border-0 bg-neutral-50") +
+          " " +
+          "transition-all duration-500 ease-[cubic-bezier(0.25,1,0.5,1)] kode-mono absolute overflow-clip z-0 p-4 flex items-center border pointer-events-none border-neutral-100 rounded-2xl"
+        }
+      >
+        <span
+          className={`text-sm flex flex-col gap-4 justify-center z-0 text-nowrap w-52 transition-all 
+           ${isOpen ? "opacity-100 h-32" : "opacity-0 h-0"} 
+            `}
+        >
+          <p className="text-neutral-400">Credits:</p>
+          <div className="flex flex-col gap-1">
+            {text.map((str) => (
+              <p>{str}</p>
+            ))}
+          </div>
+        </span>
+      </div>
     </div>
   );
 };
